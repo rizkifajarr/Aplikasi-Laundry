c4e857997ca72774059c9eb44cde6617cb54fbb7
kanadakurniawan <kanada.kurniawan@gmail.com>
2023-04-04 16:49:38 +0700

    login register

--- resources/views/layouts/backend.blade.php
+++ resources/views/layouts/backend.blade.php
@@ -606,21 +606,18 @@
                   <a href="/about" target="_blank" class="footer-link fw-bolder">Rizki, Putri & Kanada</a>
                 </div>
                 <div>
-                  <a href="https://themeselection.com/license/" class="footer-link me-4" target="_blank">License</a>
-                  <a href="https://themeselection.com/" target="_blank" class="footer-link me-4">More Themes</a>
-
                   <a
-                    href="https://themeselection.com/demo/sneat-bootstrap-html-admin-template/documentation/"
+                    href="#"
                     target="_blank"
                     class="footer-link me-4"
                     >Documentation</a
                   >
 
                   <a
-                    href="https://github.com/themeselection/sneat-html-admin-template-free/issues"
+                    href="https://github.com/rizkifajarr/Aplikasi-Laundry.git"
                     target="_blank"
                     class="footer-link me-4"
-                    >Support</a
+                    >Github</a
                   >
                 </div>
               </div>
