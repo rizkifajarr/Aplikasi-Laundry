7bde39851dc6ef5b477a1647e686d9249b88da3f
kanadakurniawan <kanada.kurniawan@gmail.com>
2023-06-17 22:11:25 +0700

    Hapus opsi ubah status admin di ubah profil

--- resources/views/dashboard/user/ubah.blade.php
+++ resources/views/dashboard/user/ubah.blade.php
@@ -15,20 +15,22 @@
                         @csrf
                         @method('patch')
                         @can('admin-gate')
-                        <div class="mb-3">
-                          <label for="exampleFormControlSelect1" class="form-label">Apakah User Ini Admin?</label>
-                          <select class="form-select" id="admin"  name="admin" aria-label="Default select example">
-                            <option selected="{{ old('admin') ?? $user->admin}}" value="{{ old('admin') ?? $user->admin}}">
-                              @if($user->admin == 1)
-                              Ya
-                              @else
-                              Tidak
-                              @endif                            
-                            </option>
-                            <option value="1">Ya</option>
-                            <option value="0">Tidak</option>
-                          </select>
-                        </div>
+                          @if ($judul <> "Ubah Profil")                            
+                            <div class="mb-3">
+                              <label for="exampleFormControlSelect1" class="form-label">Apakah User Ini Admin?</label>
+                              <select class="form-select" id="admin"  name="admin" aria-label="Default select example">
+                                <option selected="{{ old('admin') ?? $user->admin}}" value="{{ old('admin') ?? $user->admin}}">
+                                  @if($user->admin == 1)
+                                  Ya
+                                  @else
+                                  Tidak
+                                  @endif                            
+                                </option>
+                                <option value="1">Ya</option>
+                                <option value="0">Tidak</option>
+                              </select>
+                            </div>
+                          @endif
                         @endcan
                         <div class="mb-3">
                           <label class="form-label" for="basic-default-fullname">Nama Lengkap</label>
