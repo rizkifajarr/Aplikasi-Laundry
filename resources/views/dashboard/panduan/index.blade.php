95208b166b57f791a825b9a2bbd3371728aeccac
kanadakurniawan <kanada.kurniawan@gmail.com>
2023-07-10 21:01:52 +0700

    panduan admin

---
+++ resources/views/dashboard/panduan/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.admin')
+@section('judul')
+Sosial Media
+@endsection
+@section('content')
+<!-- Content -->
+
+<div class="container-xxl flex-grow-1 container-p-y">
+    
+    <!-- Hoverable Table rows -->
+    <div class="card">
+        <div class="card-body">
+            Ngetes
+        </div>
+    </div>
+    <!-- / Content -->
+    @endsection
