54c6e501bb15d84f32918aaab8a79bc7a220ddac
rizkifajarr <rizkifajarsetyawan28@gmail.com>
2023-07-16 17:59:42 +0700

    perbaiki typo, ganti gambar & update view panduan sosmed

--- resources/views/dashboard/panduan/sosmed.blade.php
+++ resources/views/dashboard/panduan/sosmed.blade.php
@@ -33,21 +33,22 @@
                                 <div class="card-body">
                                     <h5 class="card-title">Panduan - Mengedit Sosmed</h5>
                                     <p class="card-text">Pada panduan ini sebagai seorang admin anda dapat mengedit sosmed
-                                        yang digunakan pada Aplikasi Web "Loundry Kite".</p>
+                                        yang digunakan pada Aplikasi Web "Loundry Kite", dengan mengubah nama aplikasi dan
+                                        ID / Username.</p>
                                 </div>
-                                <img src="{{ asset('assets/img/panduan/faq3.png') }}" class="card-img-top border rounded"
+                                <img src="{{ asset('assets/img/panduan/sosmed3.png') }}" class="card-img-top border rounded"
                                     alt="#">
                             </div>
-                            {{-- <div class="card shadow-sm">
+                            <div class="card shadow-sm">
                                 <div class="card-body">
-                                    <h5 class="card-title">Panduan - Menghapus FAQ</h5>
-                                    <p class="card-text">Pada panduan ini sebagai seorang admin anda dapat menghapus FAQ
-                                        yaitu pertanyaan dan jawaban yang sering ditanyakan oleh customer dengan meng-klik
+                                    <h5 class="card-title">Panduan - Menghapus Sosmed</h5>
+                                    <p class="card-text">Pada panduan ini sebagai seorang admin anda dapat menghapus sosmed
+                                        yang digunakan pada Aplikasi Web "Loundry Kite" dengan meng-klik
                                         tombol "Delete" yang berwarna merah.</p>
                                 </div>
-                                <img src="{{ asset('assets/img/panduan/faq4.png') }}" class="card-img-top border rounded"
+                                <img src="{{ asset('assets/img/panduan/sosmed4.png') }}" class="card-img-top border rounded"
                                     alt="#">
-                            </div> --}}
+                            </div>
                         </div>
                     </div>
                 </div>
