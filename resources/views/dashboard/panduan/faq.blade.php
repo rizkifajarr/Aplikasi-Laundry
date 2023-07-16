96ad80b372a8665cb1c584953623d619670a3888
rizkifajarr <rizkifajarsetyawan28@gmail.com>
2023-07-16 16:56:13 +0700

    view panduan faq

--- resources/views/dashboard/panduan/faq.blade.php
+++ resources/views/dashboard/panduan/faq.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.admin')
 @section('judul')
-    Panduan Fitur Pesanan
+    Panduan Fitur FAQ
 @endsection
 @section('content')
     <div class="container-xxl flex-grow-1 container-p-y">
@@ -9,7 +9,7 @@
                 <div class="container">
                     <div class="row">
                         <div class="col">
-                            <div class="card shadow-sm mb-3">
+                            {{-- <div class="card shadow-sm mb-3">
                                 <div class="card-body">
                                     <h5 class="card-title">Panduan - Pesanan</h5>
                                     <p class="card-text">Pada panduan ini sebagai seorang admin anda dapat melihat pesanan
@@ -19,7 +19,7 @@
                                 <img src="{{ asset('assets/img/panduan/pesanan1.png') }}"
                                     class="card-img-top border rounded" alt="#">
                             </div>
-                            {{-- <div class="card shadow-sm mb-3">
+                            <div class="card shadow-sm mb-3">
                                 <div class="card-body">
                                     <h5 class="card-title">Panduan - Menambah Pesanan</h5>
                                     <p class="card-text">Pada panduan ini sebagai seorang admin anda dapat menambah jumlah
@@ -29,8 +29,8 @@
                                 </div>
                                 <img src="{{ asset('assets/img/panduan/pesanan2.png') }}"
                                     class="card-img-top border rounded" alt="#">
-                            </div> --}}
-                            {{-- <div class="card shadow-sm mb-3">
+                            </div>
+                            <div class="card shadow-sm mb-3">
                                 <div class="card-body">
                                     <h5 class="card-title">Panduan - Mengedit Pesanan</h5>
                                     <p class="card-text">Pada panduan ini sebagai seorang admin anda dapat mengedit user
@@ -39,8 +39,8 @@
                                 </div>
                                 <img src="{{ asset('assets/img/panduan/pesanan3.png') }}"
                                     class="card-img-top border rounded" alt="#">
-                            </div> --}}
-                            {{-- <div class="card shadow-sm">
+                            </div>
+                            <div class="card shadow-sm">
                                 <div class="card-body">
                                     <h5 class="card-title">Panduan - Menghapus Pesanan</h5>
                                     <p class="card-text">Pada panduan ini sebagai seorang admin anda dapat menghapus
