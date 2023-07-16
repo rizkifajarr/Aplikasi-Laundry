5d7c0a8dcb776365918f5aa06d355ca698311aab
rizkifajarr <rizkifajarsetyawan28@gmail.com>
2023-07-16 15:51:25 +0700

    view panduan dashboard

--- resources/views/dashboard/panduan/dashboard.blade.php
+++ resources/views/dashboard/panduan/dashboard.blade.php
@@ -40,7 +40,7 @@
                                 <img src="{{ asset('assets/img/panduan/paket3.png') }}" class="card-img-top border rounded"
                                     alt="#">
                             </div> --}}
-                            <div class="card shadow-sm">
+                            {{-- <div class="card shadow-sm">
                                 <div class="card-body">
                                     <h5 class="card-title">Panduan - Menghapus Paket</h5>
                                     <p class="card-text">Pada panduan ini sebagai seorang admin anda dapat menghapus
@@ -50,7 +50,7 @@
                                 </div>
                                 <img src="{{ asset('assets/img/panduan/paket4.png') }}" class="card-img-top border rounded"
                                     alt="#">
-                            </div>
+                            </div> --}}
                         </div>
                     </div>
                 </div>
