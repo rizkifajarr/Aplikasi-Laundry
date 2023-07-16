d8acc1d69329822ab5167a9eb976583d30d0f8f4
rizkifajarr <rizkifajarsetyawan28@gmail.com>
2023-07-16 14:13:41 +0700

    panduan paket

--- resources/views/dashboard/panduan/index.blade.php
+++ resources/views/dashboard/panduan/index.blade.php
@@ -26,7 +26,7 @@
                                     <button href="#" class="btn btn-outline-primary">Tampilkan</button>
                                 </div>
                             </div>
-                            {{-- <div class="card shadow-sm mb-3">
+                            <div class="card shadow-sm mb-3">
                                 <div class="card-body">
                                     <h5 class="card-title">Panduan - Paket</h5>
                                     <p class="card-text">Pada panduan ini sebagai seorang admin anda harus melihat tiap
@@ -34,7 +34,7 @@
                                     <button href="#" class="btn btn-outline-primary">Tampilkan</button>
                                 </div>
                             </div>
-                            <div class="card shadow-sm mb-3">
+                            {{-- <div class="card shadow-sm mb-3">
                                 <div class="card-body">
                                     <h5 class="card-title">Panduan - User</h5>
                                     <p class="card-text">Pada panduan ini sebagai seorang admin anda harus melihat tiap
