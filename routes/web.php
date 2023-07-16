b294acc4bbdf6e96c4828846484b11cd0ef23fce
rizkifajarr <rizkifajarsetyawan28@gmail.com>
2023-07-16 15:46:11 +0700

    nambahkan route untuk view panduan dashboard & nambahkan foto untuk view panduan dashboard

--- routes/web.php
+++ routes/web.php
@@ -85,6 +85,7 @@ Route::middleware('auth')->group(function () {
         });
         Route::prefix('dashboard/panduan')->name('panduan.')->group(function () {
             Route::get('/', function () { return view('dashboard.panduan.index'); })->name('index');
+            Route::get('/dashboard', function () { return view('dashboard.panduan.dashboard'); })->name('dashboard');
             Route::get('/user', function () { return view('dashboard.panduan.user'); })->name('user');
             Route::get('/pesanan', function () { return view('dashboard.panduan.pesanan'); })->name('pesanan');
             Route::get('/paket', function () { return view('dashboard.panduan.paket'); })->name('paket');
