98e992e11e8255e6a07aa657d3d0d22cd2504410
rizkifajarr <rizkifajarsetyawan28@gmail.com>
2023-07-16 14:07:46 +0700

    nambah view untuk panduan user

--- resources/views/dashboard/panduan/user.blade.php
+++ resources/views/dashboard/panduan/user.blade.php
@@ -10,33 +10,33 @@
                     <div class="row">
                         <div class="col">
                             <div class="card shadow-sm mb-3">
-                                <img src="{{ asset('assets/img/panduan/user1.png') }}" class="card-img-top shadow-sm"
-                                    alt="#">
                                 <div class="card-body">
                                     <h5 class="card-title">Panduan - User</h5>
                                     <p class="card-text">Pada panduan ini sebagai seorang admin anda harus melihat tiap
                                         user yang telah terdaftar seperti nama, alamat, no handphone dan email.</p>
                                 </div>
+                                <img src="{{ asset('assets/img/panduan/user1.png') }}"
+                                    class="card-img-top border-top rounded shadow-sm" alt="#">
                             </div>
-                            <div class="card shadow-sm mb-3">
-                                <img src="{{ asset('assets/img/panduan/user2.png') }}" class="card-img-top shadow-sm"
-                                    alt="#">
+                            {{-- <div class="card shadow-sm mb-3">
                                 <div class="card-body">
                                     <h5 class="card-title">Panduan - Menambah User</h5>
                                     <p class="card-text">Pada panduan ini sebagai seorang admin anda dapat menambah jumlah
                                         user dengan menyertakan nama lengkap, alamat, no handphone dan email.</p>
                                 </div>
-                            </div>
-                            <div class="card shadow-sm mb-3">
-                                <img src="{{ asset('assets/img/panduan/user3.png') }}" class="card-img-top shadow-sm"
-                                    alt="#">
+                                <img src="{{ asset('assets/img/panduan/user2.png') }}"
+                                    class="card-img-top border-top rounded shadow-sm" alt="#">
+                            </div> --}}
+                            {{-- <div class="card shadow-sm mb-3">
                                 <div class="card-body">
                                     <h5 class="card-title">Panduan - Mengedit User</h5>
                                     <p class="card-text">Pada panduan ini sebagai seorang admin anda dapat mengedit user
                                         dengan mengubah nama lengkap, alamat, no handphone dan email.</p>
                                 </div>
-                            </div>
-                            <div class="card shadow-sm">
+                                <img src="{{ asset('assets/img/panduan/user3.png') }}" class="card-img-top border-top rounded shadow-sm"
+                                    alt="#">
+                            </div> --}}
+                            {{-- <div class="card shadow-sm">
                                 <div class="card-body">
                                     <h5 class="card-title">Panduan - Menghapus User</h5>
                                     <p class="card-text">Pada panduan ini sebagai seorang admin anda dapat menghapus
@@ -46,7 +46,7 @@
                                 </div>
                                 <img src="{{ asset('assets/img/panduan/user4.png') }}"
                                     class="card-img-top border-top rounded" alt="#">
-                            </div>
+                            </div> --}}
                         </div>
                     </div>
                 </div>
