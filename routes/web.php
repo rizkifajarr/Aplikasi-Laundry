e0f9a0fe0ae1136f6c9fd8cc3f38721aafdb65c3
rizkifajarr <rizkifajarsetyawan28@gmail.com>
2023-07-16 14:05:39 +0700

    nambah route

--- routes/web.php
+++ routes/web.php
@@ -87,7 +87,7 @@ Route::middleware('auth')->group(function () {
             Route::get('/', function () { return view('dashboard.panduan.index'); })->name('index');
             Route::get('/user', function () { return view('dashboard.panduan.user'); })->name('user');
             Route::get('/pesanan', function () { return view('dashboard.panduan.pesanan'); })->name('pesanan');
-            // Route::get('/paket', function () { return view('dashboard.panduan.paket'); })->name('paket');
+            Route::get('/paket', function () { return view('dashboard.panduan.paket'); })->name('paket');
         });
     });
 });
