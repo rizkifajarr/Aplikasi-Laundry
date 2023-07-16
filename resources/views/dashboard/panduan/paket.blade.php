14e31d3bf2483aad01aae8199d4e774bb5577470
rizkifajarr <rizkifajarsetyawan28@gmail.com>
2023-07-16 15:41:52 +0700

    view panduan paket

--- resources/views/dashboard/panduan/paket.blade.php
+++ resources/views/dashboard/panduan/paket.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.admin')
 @section('judul')
-    Panduan Fitur Pesanan
+    Panduan Fitur Paket
 @endsection
 @section('content')
     <div class="container-xxl flex-grow-1 container-p-y">
